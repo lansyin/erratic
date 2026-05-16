fb803411d6a228c8952928edec8b3c55b3a7c448
Lan <catsalwaysmeow@outlook.com>
2026-05-17 03:00:50 +0800

    docs: elaborate the behavior of Nae

--- src/nae.rs
+++ src/nae.rs
@@ -4,8 +4,9 @@ use std::{
     fmt::{self, Display},
 };
 
-/// Not an error, a zero-sized error placeholder for [Error][crate::Error], represents the end of the error source chain.
-/// It is used by [`Builder`][crate::Builder] and will not appear in the [`chain`][crate::Error::chain] method.
+/// Not an error, a zero-sized error placeholder for [`Builder`][crate::Builder], also represents
+/// the end of source chain in [`Error`][crate::Error]. It is used by [`Builder`][crate::Builder]
+/// and will not appear in the iterator returned by [`chain`][crate::Error::chain].
 #[derive(Debug)]
 pub struct Nae;
 
